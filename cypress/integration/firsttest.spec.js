37896eae46fbebaebfb58e4f6ddf35c737917d29
koval500 <kovalenko.04012001@gmail.com>
2022-06-28 18:57:32 +0300

    add master commit

--- cypress/integration/firsttest.spec.js
+++ cypress/integration/firsttest.spec.js
@@ -43,7 +43,10 @@
 // Comment 
 
 //Comment
+ 
+//Comment
 
+//Comment
  it('Using Get with Find and Eq', ()=> {
     cy.visit('https://next.privat24.ua/deposit/open') 
     cy.get('tbody').find('td').find('div').find('button').eq('0')
